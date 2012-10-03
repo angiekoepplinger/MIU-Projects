f1373b8cd1dc91dd5902096e6c3b8d7a3dc9715b
angiekoepplinger <angiekoepplinger@fullsail.edu>
2012-10-02 20:37:50 -0500

    Push JSON file

---
+++ Koepplinger_Angela_Project2/gold/js/json.js
@@ -0,0 +1,40 @@
+//Angela Koepplinger
+//Visual Frameworks: Term 1209
+// Project 4: Web App Part 4
+//Date: 09/20/12
+
+
+var json = {
+	"task1": {
+		"purchaseType": 	["Purchase Type:", "Product"],
+		"workOrder": 		["Work Order:", "SRVR Upgrade"],
+		"supportSite": 		["Support Site:", "HSV"],
+		"itemType": 		["Item Type:", "Hardware"],
+		"asset": 			["Asset:", "Yes"],
+		"manufacturer": 	["Manufacturer:", "Supermicro"],
+		"partNumber": 		["Part Number:", "SYS-1026T-URF"],
+		"qty": 				["Qty:", "100"],
+		"urgency": 			["Urgency:", "8"],
+		"mgmtApproval": 	["Management Approval:", "Yes"],
+		"rrRequest": 		["R&R Request:", "Yes"],
+		"dateRequired": 	["Date Required:", "2013-03-01"],
+		"completionDate": 	["Completion Date:", "2012-12-31"],
+		"notes": 			["Notes:", "Task Completed"]
+	},
+	"task2": {
+		"purchaseType": 	["Purchase Type:", "Maintenance"],
+		"workOrder": 		["Work Order:", "TES/SM"],
+		"supportSite": 		["Support Site:", "TES"],
+		"itemType": 		["Item Type:", "Support Contract"],
+		"asset": 			["Asset:", "No"],
+		"manufacturer": 	["Manufacturer:", "Cisco Systems"],
+		"partNumber": 		["Part Number:", "CON-SNT"],
+		"qty": 				["Qty:", "2000"],
+		"urgency": 			["Urgency:", "7"],
+		"mgmtApproval": 	["Management Approval:", "Yes"],
+		"rrRequest": 		["R&R Request:", "Yes"],
+		"dateRequired": 	["Date Required:", "2013-01-01"],
+		"completionDate": 	["Completion Date:", "2012-12-15"],
+		"notes": 			["Notes:", "Task Completed"]
+	}
+};
