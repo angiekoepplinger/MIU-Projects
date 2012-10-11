7a1fba6ffa352e0bd6443f41560e6d95b1b5c0e8
angiekoepplinger <angiekoepplinger@fullsail.edu>
2012-10-10 21:28:43 -0500

    begin validation

---
+++ Koepplinger_Angela_Project3/main.js
@@ -0,0 +1,16 @@
+/*Angela Koepplinger
+MIU: Term 1210
+Project 3: Web App
+Date: 10/10/12*/
+
+
+
+/*$('#home').on('pageinit', function(){
+	//code needed for home page goes here
+});	*/
+		
+$(#home).on('pageinit', function(){
+
+	var myform = $("#procurementTask");
+	myform.validate();
+});
